1805ff7db9821129d1c5b2f0b8fbd762b613c267
HP <hamzaatd73@gmail.com>
2021-02-21 00:00:01 +0500

    hello

---
+++ app/src/main/java/com/maliksoft/github_test_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maliksoft.github_test_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
